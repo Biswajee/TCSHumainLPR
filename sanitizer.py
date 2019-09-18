43c79a4c4fa64e3d4543fc44e8a48023a187097c
Biswajee <roy.biswajeet161@gmail.com>
2019-09-19 01:44:14 +0530

    fixed minor errors

--- sanitizer.py
+++ sanitizer.py
@@ -27,50 +27,89 @@ def processing(plate_list):
 
     print('processing images...')
     for img in plate_list:
-        # Maximize Contrast
-        structElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-        imgTopHat = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, structElement)
-        imgBlackHat = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, structElement)
-        imgGrayscalePlusTopHat = cv2.add(img, imgTopHat)
-        img = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
+        try:
+            print('Original Image Shape',img.shape)
+            
+            # Grayscale
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            print('Grayscale Image Shape',img.shape)
 
-        # Adaptive Thresholding
-        img_blurred = cv2.GaussianBlur(img, ksize=(5, 5), sigmaX=0)
-        img_thresh = cv2.adaptiveThreshold(
-            img_blurred, 
-            maxValue=255.0, 
-            adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
-            thresholdType=cv2.THRESH_BINARY_INV, 
-            blockSize=19, 
-            C=0
-        )
+            # Maximize Contrast
+            structElement = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+            imgTopHat = cv2.morphologyEx(img, cv2.MORPH_TOPHAT, structElement)
+            imgBlackHat = cv2.morphologyEx(img, cv2.MORPH_BLACKHAT, structElement)
+            imgGrayscalePlusTopHat = cv2.add(img, imgTopHat)
+            img = cv2.subtract(imgGrayscalePlusTopHat, imgBlackHat)
 
-        # Find Contours
-        _, contours, _ = cv2.findContours(
-            img_thresh, 
-            mode=cv2.RETR_LIST, 
-            method=cv2.CHAIN_APPROX_SIMPLE
-        )
-        temp_result = np.zeros((height, width, channel), dtype=np.uint8)
-        cv2.drawContours(temp_result, contours=contours, contourIdx=-1, color=(255, 255, 255))
+            # Adaptive Thresholding
+            img_blurred = cv2.GaussianBlur(img, ksize=(5, 5), sigmaX=0)
+            img_thresh = cv2.adaptiveThreshold(
+                img_blurred, 
+                maxValue=255.0, 
+                adaptiveMethod=cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+                thresholdType=cv2.THRESH_BINARY_INV, 
+                blockSize=19, 
+                C=9
+            )
 
-        # Prepare Data
-        temp_result = np.zeros((height, width, channel), dtype=np.uint8)
-        contours_dict = []
-        for contour in contours:
-            x, y, w, h = cv2.boundingRect(contour)
-            cv2.rectangle(temp_result, pt1=(x, y), pt2=(x+w, y+h), color=(255, 255, 255), thickness=1)
-            # insert to dict
-            contours_dict.append({
-                'contour': contour,
-                'x': x,
-                'y': y,
-                'w': w,
-                'h': h,
-                'cx': x + (w / 2),
-                'cy': y + (h / 2)
-            })
-        Sanitized_list.append(temp_result)
+            # Find Contours
+            _, contours, _ = cv2.findContours(
+                img_thresh, 
+                mode=cv2.RETR_LIST, 
+                method=cv2.CHAIN_APPROX_SIMPLE
+            )
+            temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+            cv2.drawContours(temp_result, contours=contours, contourIdx=-1, color=(255, 255, 255))
+
+            # Prepare Data
+            temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+            contours_dict = []
+            for contour in contours:
+                x, y, w, h = cv2.boundingRect(contour)
+                cv2.rectangle(temp_result, pt1=(x, y), pt2=(x+w, y+h), color=(255, 255, 255), thickness=1)
+                # insert to dict
+                contours_dict.append({
+                    'contour': contour,
+                    'x': x,
+                    'y': y,
+                    'w': w,
+                    'h': h,
+                    'cx': x + (w / 2),
+                    'cy': y + (h / 2)
+                })
+
+
+            # Eliminating contours with irrelevant sizes 
+            MIN_AREA = 80
+            MIN_WIDTH, MIN_HEIGHT = 2, 8
+            MIN_RATIO, MAX_RATIO = 0.25, 1.0
+
+            possible_contours = []
+
+            cnt = 0
+            for d in contours_dict:
+                area = d['w'] * d['h']
+                ratio = d['w'] / d['h']
+                
+                if area > MIN_AREA \
+                and d['w'] > MIN_WIDTH and d['h'] > MIN_HEIGHT \
+                and MIN_RATIO < ratio < MAX_RATIO:
+                    d['idx'] = cnt
+                    cnt += 1
+                    possible_contours.append(d)
+                    
+            # visualize possible contours
+            temp_result = np.zeros((height, width, channel), dtype=np.uint8)
+
+            for d in possible_contours:
+            #     cv2.drawContours(temp_result, d['contour'], -1, (255, 255, 255))
+                cv2.rectangle(temp_result, pt1=(d['x'], d['y']), pt2=(d['x']+d['w'], d['y']+d['h']), color=(255, 255, 255), thickness=2)
+
+
+
+            Sanitized_list.append(temp_result)
+        except:
+            pass
     return Sanitized_list
         
 
@@ -82,7 +121,12 @@ plot() : accepts an input image and displays plot on screen.
 [pauses subsquent code executions] 
 '''
 def plot(img_list):
+    i = 0
     for img in img_list:
-        plt.figure(figsize=(12, 10))
-        plt.imshow(img, cmap='gray')
-        plt.show()
+        _ , ax = plt.subplots(1, 2) # horizontally stacked subplots
+        plt.figure()
+        ax[0].imshow(img, cmap='gray')
+        ax[1].imshow(img, cmap='gray')
+        plt.savefig('fig_' + str(i), transparent=True, bbox_inches='tight', dpi=30)
+        i=i+1
+    plt.show()
